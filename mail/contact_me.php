64c1a7b1a6fa7de10261e0d8bad57025e2d68e9c
Sandeep <stiwari@berkeley.edu>
2018-06-03 22:44:24 -0700

    fix form

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -13,7 +13,7 @@ $name = $_POST['name'];
 $email = $_POST['email'];
 $message = $_POST['message'];
 
-$to = 'stiwari@berkeley.edu';
+$to = "stiwari@berkeley.edu";
 $email_subject = "$name sent you a message!";
 $email_body = "You have received a new essage from your website contact form. \n\n". "Here are the details: \n\nName: $name\n\nEmail: $email\n\nMessage:\n$message";
 $headers = "From: noreply@berkeley.edu";
